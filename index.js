e44c6bb320b649edfb6598b4ac72612d87b7a1e1
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-11 19:49:31 -0300

    up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+require("dotenv").config();
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.render("index");
