9c51f3470b3ab3b2552282bc75158123d71167ba
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-11 22:21:17 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const db = require;
+const db = require("./model/database");
 const port = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
