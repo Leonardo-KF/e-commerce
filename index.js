984d7889a35f0199436eeeb75a720df3ac6f938d
FredyyLima <fredyy_lima@live.com>
2021-10-15 22:34:28 -0300

    confirmar

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 require("dotenv").config();
 const db = require("./model/database");
 const port = process.env.PORT;
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); 
 app.set("view engine", "ejs");
+
 
 app.get("/", function (req, res) {
   res.render("index");
