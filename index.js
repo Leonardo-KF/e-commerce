534e45722c98b62da553fc2a5298881caee6524e
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-17 01:36:34 -0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-const Sequelize = require("sequelize");
-require("dotenv").config();
-const db = require("./model/database");
-const port = process.env.PORT;
-app.use(express.urlencoded({ extended: true }));
 const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "views/public")));
 app.set("view engine", "ejs");
 
 app.listen(3000);
