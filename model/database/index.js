d7cd8c8514e0ab00b57c81419c2f3b36151cf85e
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-17 00:03:29 -0300

    update

--- model/database/index.js
+++ model/database/index.js
@@ -1,15 +1,7 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(DATA_URL);
 
 async function conectado() {
   try {
---
+++ model/logins.js
@@ -0,0 +1,18 @@
+const db = require("./../database");
+
+const login = database.define("logins", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
