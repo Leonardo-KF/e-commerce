751532618babcad6fd1cfa4fe342105d3f1773e4
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-20 01:10:15 -0300

    update

--- controller/routes.js
+++ controller/routes.js
@@ -24,7 +24,6 @@ const upload = multer({ storage });
 
 route.get("/", async (req, res) => {
   const items = await produtos.findAll();
-  console.log(items);
   res.render("index", { items: items });
 });
 
