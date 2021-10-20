d1efb642bedb4042d4ad5eb5edec5b2e2070d7b1
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-20 04:35:33 -0300

    update

--- views/public/script.js
+++ views/public/script.js
@@ -1,10 +1,16 @@
 const el = document.querySelector(".home-section .modelo-area .models-produto");
 const as = document.querySelectorAll(".allas");
+const popup = document.getElementById("window-area");
 const c = (elemt) => document.querySelector(elemt);
 
 as.forEach((element) => {
   element.addEventListener("click", (e) => {
+    console.log("rodou");
     e.preventDefault();
-    c(".modelsWindowArea").style.display = "flex";
+    popup.style.opacity = 0;
+    popup.style.display = "flex";
+    setTimeout(() => {
+      popup.style.opacity = 1;
+    }, 200);
   });
 });
