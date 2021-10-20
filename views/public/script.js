da217397f1a095808ba436dd847775844576aa12
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-20 19:57:03 -0300

    update

--- views/public/script.js
+++ views/public/script.js
@@ -1,9 +1,3 @@
-async () => {
-  const produtos = await axios.get("http://localhost:3000/info");
-  const produto = produtos.data;
-  console.log(produto);
-};
-
 const el = document.querySelector(".home-section .modelo-area .models-produto");
 const as = document.querySelectorAll(".allas");
 const c = (elemt) => document.querySelector(elemt);
@@ -11,7 +5,6 @@ const c = (elemt) => document.querySelector(elemt);
 as.forEach((element) => {
   element.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(element.id);
     const popup = document.getElementById("w" + element.id);
     popup.style.opacity = 0;
     popup.style.display = "flex";
