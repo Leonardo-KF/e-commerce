35a062416b35cba0b83fa7d2060ba555e3d6c630
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-10-20 21:49:18 -0300

    update

--- views/public/script.js
+++ views/public/script.js
@@ -1,15 +1,22 @@
 const el = document.querySelector(".home-section .modelo-area .models-produto");
 const as = document.querySelectorAll(".allas");
-const c = (elemt) => document.querySelector(elemt);
 
 as.forEach((element) => {
   element.addEventListener("click", (e) => {
     e.preventDefault();
     const popup = document.getElementById("w" + element.id);
+    const bot = document.getElementById("voltarflex" + element.id);
     popup.style.opacity = 0;
     popup.style.display = "flex";
     setTimeout(() => {
       popup.style.opacity = 1;
     }, 200);
+    bot.addEventListener("click", () => {
+      popup.style.opacity = 1;
+      setTimeout(() => {
+        popup.style.opacity = 0;
+      }, 200);
+      popup.style.display = "none";
+    });
   });
 });
